bd5247e71d8914031db6279d5bd78e2c8deac718
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-03-17 08:06:23 +0200

    Add javascript for toggling hide password

--- MidnightArchive/wwwroot/js/site.js
+++ MidnightArchive/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.toggle-password').forEach(icon => {
+        icon.addEventListener('click', () => {
+            const input = icon.parentElement.querySelector('input');
+            input.type = input.type === 'password' ? 'text' : 'password';
+        });
+    });
+});
