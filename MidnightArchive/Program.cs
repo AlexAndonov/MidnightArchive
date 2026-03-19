c87b392173f7ea9cc8851842a606c4564c4f98a2
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-03-20 00:20:22 +0200

    Add UseAuthentication in program.cs

--- MidnightArchive/Program.cs
+++ MidnightArchive/Program.cs
@@ -26,7 +26,6 @@ namespace MidnightArchive
 
             var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseMigrationsEndPoint();
@@ -34,13 +33,13 @@ namespace MidnightArchive
 			else
 			{
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
 			app.UseHttpsRedirection();
 			app.UseRouting();
 
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.MapStaticAssets();
