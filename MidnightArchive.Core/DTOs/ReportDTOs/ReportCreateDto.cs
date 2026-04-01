f9b979d4836656e6574518c9cbb7ff265730d1a0
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-04-01 23:20:16 +0300

    Add report DTOs

---
+++ MidnightArchive.Core/DTOs/ReportDTOs/ReportCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using MidnightArchive.Infra.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+using static MidnightArchive.Infra.Data.Constants.ValidationConstants;
+
+namespace MidnightArchive.Core.DTOs.ReportDTOs
+{
+	public class ReportCreateDto
+	{
+		[Required(ErrorMessage = FieldRequiredErrorMessage)]
+		public Guid StoryId { get; set; }
+
+		[Required(ErrorMessage = FieldRequiredErrorMessage)]
+		public ReportReason Reason { get; set; }
+
+		[MaxLength(ReportDescriptionMaxLength)]
+		public string? Description { get; set; }
+	}
+}
---
+++ MidnightArchive.Core/DTOs/ReportDTOs/ReportListDto.cs
@@ -0,0 +1,26 @@
+﻿using MidnightArchive.Infra.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MidnightArchive.Core.DTOs.ReportDTOs
+{
+	public class ReportListDto
+	{
+		public Guid Id { get; set; }
+
+		public Guid StoryId { get; set; }
+
+		public string StoryTitle { get; set; } = null!;
+
+		public string ReporterName { get; set; } = null!;
+
+		public ReportReason Reason { get; set; }
+
+		public string? Description { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+
+		public bool IsResolved { get; set; }
+	}
+}
