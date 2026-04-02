982c549474d00601821a310187a7b9ab6b0a8d45
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-04-02 21:49:24 +0300

    Add StoryLike entity and configure many-to-many relationship with Story and User

--- MidnightArchive.Infra/Data/Migrations/20260402181013_AddStoryLike.Designer.cs
+++ MidnightArchive.Infra/Data/Migrations/20260402181013_AddStoryLike.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MidnightArchive.Data;
 
@@ -11,9 +12,11 @@ using MidnightArchive.Data;
 namespace MidnightArchive.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260402181013_AddStoryLike")]
+    partial class AddStoryLike
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MidnightArchive.Infra/Data/Migrations/20260402181013_AddStoryLike.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MidnightArchive.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStoryLike : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StoryLikes",
+                columns: table => new
+                {
+                    StoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StoryLikes", x => new { x.StoryId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_StoryLikes_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_StoryLikes_Stories_StoryId",
+                        column: x => x.StoryId,
+                        principalTable: "Stories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StoryLikes_UserId",
+                table: "StoryLikes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StoryLikes");
+        }
+    }
+}
