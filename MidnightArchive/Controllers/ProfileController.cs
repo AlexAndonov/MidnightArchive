3361d32329ac3bb62853ce5f519be34e4d9f2753
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-04-06 22:25:53 +0300

    Add profile controller

---
+++ MidnightArchive/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MidnightArchive.Core.Contracts;
+using MidnightArchive.Core.DTOs.HomeDTOs;
+using System.Security.Claims;
+
+namespace MidnightArchive.Controllers
+{
+	[Authorize]
+	public class ProfileController : Controller
+	{
+		private readonly IProfileService profileService;
+
+		public ProfileController(IProfileService _profileService)
+		{
+			profileService = _profileService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> MyProfile()
+		{
+			string? userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+			if (string.IsNullOrWhiteSpace(userId))
+			{
+				return Unauthorized();
+			}
+
+			bool isAdmin = User.IsInRole("Admin");
+
+			UserProfileDto? model = await profileService.GetMyProfileAsync(userId, isAdmin);
+
+			if (model == null)
+			{
+				return NotFound();
+			}
+
+			return View(model);
+		}
+	}
+}
