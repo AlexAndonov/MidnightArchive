28d50f00c5dfa02add7b7720bcb603d0efbcdbe4
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-04-06 22:40:37 +0300

    Add admin area with controller and view

---
+++ MidnightArchive/Areas/Admin/Controllers/ReportsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MidnightArchive.Core.Contracts;
+using MidnightArchive.Core.DTOs.ReportDTOs;
+
+namespace MidnightArchive.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
+	public class ReportsController : Controller
+	{
+		private readonly IAdminService adminService;
+
+		public ReportsController(IAdminService _adminService)
+		{
+			adminService = _adminService;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			IEnumerable<ReportedStoryAdminDto> reports = await adminService.GetReportedStoriesAsync();
+
+			return View(reports);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Resolve(Guid storyId)
+		{
+			await adminService.ResolveReportsAsync(storyId);
+
+			return RedirectToAction("Index", "Reports", new { area = "Admin" });
+		}
+	}
+}
