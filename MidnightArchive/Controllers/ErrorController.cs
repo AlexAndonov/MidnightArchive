a348b3cdee26493aff2e2a231348635385336013
AlexAndonov <114021498+AlexAndonov@users.noreply.github.com>
2026-04-06 22:27:09 +0300

    Add custom error views

---
+++ MidnightArchive/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MidnightArchive.Models;
+using System.Diagnostics;
+
+namespace MidnightArchive.Controllers
+{
+	public class ErrorController : Controller
+	{
+		[Route("Error/404")]
+		public IActionResult NotFoundPage()
+		{
+			Response.StatusCode = 404;
+			return View("NotFound");
+		}
+
+		[Route("Error/500")]
+		public IActionResult InternalServerError()
+		{
+			Response.StatusCode = 500;
+			return View("InternalServerError");
+		}
+
+		[Route("Error")]
+		public IActionResult HandleError(int? statusCode = null)
+		{
+			if (statusCode == 404 || statusCode == 400)
+			{
+				Response.StatusCode = statusCode ?? 404;
+				return View("NotFound");
+			}
+
+			Response.StatusCode = 500;
+			return View("InternalServerError");
+		}
+	}
+}
